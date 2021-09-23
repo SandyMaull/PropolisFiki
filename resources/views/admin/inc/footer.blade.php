a87612b4efa3aee01833b9adc6e94aaffbb3a90e
SandyMaull <sandymaulana0@gmail.com>
2021-09-23 13:46:11 +0800

    Einzigartig

--- resources/views/admin/inc/footer.blade.php
+++ resources/views/admin/inc/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2021 <a href="http://kuronekosan.web.id">Kuronekosan</a>.</strong>
+    <strong>Copyright &copy; 2021 <a href="https://www.propolisku.id">Propolisku</a>, Powered By <a href="http://kuronekosan.web.id">Kuronekosan</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 0.0.1
